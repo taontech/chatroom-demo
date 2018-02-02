7746407a1243cf5798cba62b1ff6d17ab09f4015
taoning <taoning@wanda.cn>
2018-02-02 17:41:31 +0800

    xx

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,8 +17,23 @@ $(function () {
         // 滚动条滚动到底部
         scrollToBottom();
     });
+    socket.on('listchange', function (msg) {
+        $('.messages').append('<p>'+msg+'</p>');
+        // 滚动条滚动到底部
+        scrollToBottom();
+    });
     socket.on('new message', function (msg,user) {
-        $('.messages').append('<p>'+user+'说：'+msg+'</p>');
+        $('.messages').append('<font color="green">' + '<p>'+user+'说：'+msg+'</p>' +'</font>');
+        // 滚动条滚动到底部
+        scrollToBottom();
+    });
+    socket.on('sys message', function (msg,user) {
+        $('.messages').append('<font color="red">' + '<p>'+msg+'</p>' +'</font>');
+        // 滚动条滚动到底部
+        scrollToBottom();
+    });
+    socket.on('private message', function (msg,user,toUser) {
+        $('.messages').append('<font color="blue">' +'<p>'+ user+'悄悄对'+toUser+'说：'+msg+'</p>' +'</font>');
         // 滚动条滚动到底部
         scrollToBottom();
     });
@@ -30,6 +45,9 @@ $(function () {
             }
             socket.send(message);
             $(this).val('');
+        }else if( e.which ===192 )
+        {
+            socket.emit('list','');
         }
     });
 });
--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.use('/users', users);
 var server = http.Server(app);
 var io = require('socket.io').listen(server);
 var roomUser = {};
+var nameSockets = [];
+
 io.on('connection', function (socket) {
 
     // 获取用户当前的url，从而截取出房间id
@@ -31,29 +33,96 @@ io.on('connection', function (socket) {
     var split_arr = url.split('/');
     var roomid = split_arr[split_arr.length-1] || 'index';
     var user = '';
-
     socket.on('join', function (username) {
          user = username;
         // 将用户归类到房间
         if (!roomUser[roomid]) {
             roomUser[roomid] = [];
         }
+        if(!nameSockets[roomid])
+        {
+            nameSockets[roomid] = {};
+        }
+        nameSockets[roomid][user] = socket;
+
         roomUser[roomid].push(user);
         socket.join(roomid);
         socket.to(roomid).emit('sys', user + '加入了房间');
         socket.emit('sys',user + '加入了房间');
+
+        var lists = '现在群中有：';
+        for(var i = 0; i<roomUser[roomid].length; i++)
+        {
+            lists = lists + roomUser[roomid][i] +  ",";
+        }
+        socket.to(roomid).emit('listchange', lists);
+        socket.emit('listchange', lists);
+
+    });
+
+    socket.on('message', function (msg) {
+        // 验证如果用户不在房间内则不给发送
+        if (roomUser[roomid].indexOf(user)< 0) {
+            return false;
+        }
+
+        var msgarray = msg.split("");
+        if(msgarray[0] === '*')
+        {
+            var toid = [];
+            var tomsg = [];
+            var nameDone = false;
+           //  toid[0] = msg.split("")[1];
+            for( var i = 1; i< msgarray.length; ++i )
+            {
+                if(msgarray[i] === " ")
+                    nameDone = true;
+                if( !nameDone )
+                    toid.push(msgarray[i]);
+                else
+                    tomsg.push(msgarray[i]);
+            }
+            console.log(tomsg.join("")+"  "+toid.join(""));
+            var ps = nameSockets[roomid][toid.join("")];
+            if(ps)
+            {
+                ps.emit('private message', tomsg.join(""),user,"你");
+                socket.emit('private message', tomsg.join(""),"你",toid.join(""));
+
+            }
+        }else if( msg === "/roll" )
+        {
+            var count = Math.random()*100;
+            socket.to(roomid).emit('sys message', user+"  roll了"+ Math.floor(count)+"点","系统");
+            socket.emit('sys message', user+"  roll了"+Math.floor(count)+"点","系统");
+        }
+        else
+        {
+            socket.to(roomid).emit('new message', msg,user);
+            socket.emit('new message', msg,user);
+        }
+
     });
 
     // 监听来自客户端的消息
-    socket.on('message', function (msg) {
-        // 验证如果用户不在房间内则不给发送
-        if (roomUser[roomid].indexOf(user)< 0) {  
-          return false;
+    socket.on('list', function (msg) {
+        var lists = '现在群中有：';
+        for(var i = 0; i<roomUser[roomid].length; i++)
+        {
+            lists = lists + roomUser[roomid][i] +  ",";
         }
-        socket.to(roomid).emit('new message', msg,user);
-        socket.emit('new message', msg,user);
+     //   socket.to(roomid).emit('listchange', lists);
+        socket.emit('listchange', lists);
     });
-
+    socket.on('roll', function (msg) {
+        var lists = '现在群中有：';
+        for(var i = 0; i<roomUser[roomid].length; i++)
+        {
+            lists = lists + roomUser[roomid][i] +  ",";
+        }
+        //   socket.to(roomid).emit('listchange', lists);
+        socket.emit('listchange', lists);
+    });
     // 关闭
     socket.on('disconnect', function () {
         // 从房间名单中移除
